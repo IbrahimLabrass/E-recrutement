3550c991880e599ba0565d9722778818a30073f7
Oussama95el <oussamaelbechari@gmail.com>
2022-12-30 11:40:32 +0100

    setup workspace

---
+++ myRh_backend/src/main/java/com/simplon/myRh_backend/MyRhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.myRh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRhBackendApplication.class, args);
+	}
+
+}
